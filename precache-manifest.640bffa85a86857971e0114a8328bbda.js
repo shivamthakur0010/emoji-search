fb4a90b7e903ae88b7543dde301ded58a9a8aef3
shivamthakur0010 <demonking0010@gmail.com>
2023-04-25 21:30:41 +0530

    Updates

---
+++ precache-manifest.640bffa85a86857971e0114a8328bbda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4ff538d18dfb9c75316a57b81a1fc0",
+    "url": "/emoji-search/index.html"
+  },
+  {
+    "revision": "209065ae475ebd6a6b2a",
+    "url": "/emoji-search/static/css/main.65605343.chunk.css"
+  },
+  {
+    "revision": "7707cbf631ccc88eb134",
+    "url": "/emoji-search/static/js/2.8a7ab880.chunk.js"
+  },
+  {
+    "revision": "de0a1713895ef548baca42cd10aace43",
+    "url": "/emoji-search/static/js/2.8a7ab880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "209065ae475ebd6a6b2a",
+    "url": "/emoji-search/static/js/main.5ad73e98.chunk.js"
+  },
+  {
+    "revision": "d44d1531576180c634c7",
+    "url": "/emoji-search/static/js/runtime-main.d8cd5ee4.js"
+  }
+]);
